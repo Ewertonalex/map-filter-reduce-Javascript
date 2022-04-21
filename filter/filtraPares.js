42290c517de67fdd738f0c8b43df7020b7617f1e
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2022-04-21 20:57:16 -0300

    Add files via upload

--- filter/filtraPares.js
+++ filter/filtraPares.js
@@ -1,9 +1,11 @@
 function filtraPares(arr) {
-	if (!arr || !arr.length) return;
-
-	const filteredArr = arr.filter((item) => item % 2 === 0);
-
-	return filteredArr;
+  return arr.filter(callback);
 }
 
-console.log(filtraPares([1, 2, 3, 4]));
+function callback(item) {
+  return item % 2 === 0;
+}
+
+const meuArray = [1, 23, 55, 67, 30, 2, 4];
+
+console.log(filtraPares(meuArray));
